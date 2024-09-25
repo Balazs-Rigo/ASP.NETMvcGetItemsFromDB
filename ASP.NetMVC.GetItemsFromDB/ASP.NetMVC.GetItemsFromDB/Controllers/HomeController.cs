80f6f888210ddde42b65abed7e9855e468d5691f
balazsrigo <rigo.balazs86@gmail.com>
2024-09-25 20:25:34 +0200

    init

---
+++ ASP.NetMVC.GetItemsFromDB/ASP.NetMVC.GetItemsFromDB/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using ASP.NetMVC.GetItemsFromDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NetMVC.GetItemsFromDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetComments()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NetMVC.GetItemsFromDB/ASP.NetMVC.GetItemsFromDB/Models/Comment.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NetMVC.GetItemsFromDB.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+    }
+}
